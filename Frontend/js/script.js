cdbaf22a5e33d77de1f378332246d31efcea43b7
wi21b030 <wi21b030@technikum-wien.at>
2023-04-23 18:37:38 +0200

    dbaccess added, dynamic navbar, registration form

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,1 +1,28 @@
+$(document).ready(function () {
+ 
+  $('nav a').on('click', function (event) {
+    event.preventDefault();
+    var url = $(this).attr('href');
+    $.get(url, function (data) {
+      var $newContent = $('<div>').html(data);
+      $('#content').html($newContent.find('#content').html());
+      if (!$('nav').length) {
+        $('body').prepend($newContent.find('nav'));
+      }
+    });
+  });
 
+  $('#registration-form').submit(function (event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+    $.ajax({
+      type: 'POST',
+      url: '../Backend/logic/requestHandler.php',
+      data: formData,
+      success: function (response) {
+      },
+      error: function (xhr, status, error) {
+      }
+    });
+  });
+});
