f97fa7bea7c32810d0241e4bd6965bc526233a1d
wi21b030 <wi21b030@technikum-wien.at>
2023-05-14 19:38:28 +0200

    remove old script

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-    
+    /*
+    note for self/team:
+     - dynamic loading of pages getting messy
+     - script getting long
+     - navbar logic getting complicated
+     - when I log in content of login-form is saved and when I refresh because of local storage it gets loaded back in
+
+    possible solution:
+    - keep navbar same everywhere -> find way to keep navbar but change page below maybe
+    - seperate scripts i.e. register.js, login.js
+    - keep common functionality in script.js
+    - move to MPA, but dynamic loading of content still via scripts
+    - create seperate columns/divs in index.html for components such as login
+      -> then load those or hide them -> more SPA-affirm --> see modals, icons in nav etc
+
+    further issues/ideas/solutions:
+     - ...
+  */
 });
